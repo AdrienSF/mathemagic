950183f796d025638aca2d2c6dbcb8c9529556d1
Adrien Schurger-Foy <adriensf@gmail.com>
2021-02-01 11:37:30 -0800

    add teaser.py

--- secret_number.py
+++ secret_number.py
@@ -6,15 +6,26 @@ class Performer():
         self.choices = choices
         self.check_dict = self.get_check_dict()
 
-    # given all but one non-zero digit, return find the last digit and return all the digits
+    # given all but one non-zero digit, find the last digit and return all the digits
     def get_digits(self, revealed_digits: str):
-        revealed_digits = sorted([ int(d) for d in revealed_digits], reverse=True)
-        last_digit = 9 - (sum(revealed_digits) % 9)
-        if last_digit == 9:
-            print('warning: hidden digit could be 0 OR 9. Continuing with the assumption that it is 9')
-        digits = int("".join([str(last_digit)] + [str(d) for d in revealed_digits]))
 
+        revealed_digits_list = sorted([ int(d) for d in revealed_digits], reverse=True)
+        last_digits_sum = 9 - (sum(revealed_digits_list) % 9)
+        if last_digits_sum == 9:
+            print('warning: hidden digit could be 0 OR 9. Continuing with the assumption that it is 9')
+
+        digits = int("".join([str(last_digits_sum)] + revealed_digits_list))
         return digits
+
+
+    # given some digits and the total number of digits, return the possible digits
+    def get_pos_digits(self, revealed_digits: list, total_digits: int):
+        total_last_digits = total_digits - len(revealed_digits)
+        digit_set = set(range(pow(10, total_last_digits-1), pow(10, total_last_digits)))
+        last_digits_pos = [ revealed_digits + [int(d) for d in str(pos)] for pos in digit_set if sum(revealed_digits + [int(d) for d in str(pos)]) % 9 == 0 ]
+
+        return last_digits_pos
+
 
     # this is a way of eliminating further possibilities
     def crosscheck(self, to_check: set, check_dict=None):
@@ -31,6 +42,7 @@ class Performer():
     def get_possibilities(self, digits: int, revealed_num=None, r=100):
         if not revealed_num:
             revealed_num = self.revealed_num
+
         digits = [d for d in str(digits)]
         permutations = list(set([int(num) for num in ["".join(permutation) for permutation in list(itertools.permutations(digits))]]))
         possibilities1 = [int(num/revealed_num) for num in permutations if num % revealed_num == 0 and num/revealed_num < r]
