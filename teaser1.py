9671ca12c555b93f199af8ac351d335e79296075
Adrien Schurger-Foy <adriensf@gmail.com>
2021-02-16 13:36:36 -0800

    add list of good numbers to teaser1.py

---
+++ teaser1.py
@@ -0,0 +1,35 @@
+from secret_number import Performer
+
+def get_possibilities(revealed_num, revealed_digits, total_digits):
+    perf = Performer(revealed_num, list(range(20)))
+
+
+
+
+    # get possible digits
+    possible_digits = perf.get_pos_digits(revealed_digits, total_digits)
+
+    # get possible secret num for every possible set of digits
+    possibilities = set([])
+    for digits in possible_digits:
+        pos = perf.get_possibilities(int(''.join([str(d) for d in digits])))
+        possibilities = possibilities.union(pos)
+
+    # remove 1 digit numbers from the possibilities
+    possibilities = [pos for pos in possibilities if len(str(pos)) > 1]
+    return possibilities
+
+
+# ask the performer to select their number from a list of good options
+top_choices = [9009, 30303, 45045, 18018, 36036, 70101, 90009, 90090]
+
+for i in  range(len(top_choices)):
+    print('choice ' + str(i+1) + ': ' + str(top_choices[i]))
+revealed_num = top_choices[int(input('enter choice: '))-1]
+# input digits
+revealed_digits = [ int(d) for d in input('enter revealed digits(no spaces): ') ]
+total_digits = int(input('enter total number of digits: '))
+possibilities = get_possibilities(revealed_num, revealed_digits, total_digits)
+
+print('possible secret numbers: ')
+print(possibilities)
